c80d43685f9b3a019fd0b4b6e98df6529762cf0e
Akhmedovdeveloper01 <developerakhmedov@gmail.com>
2025-01-08 12:31:48 +0500

    add all changes

---
+++ js/data.js
@@ -0,0 +1,68 @@
+export const skills = [
+    {
+        id: 1,
+        title: "HTML",
+        icon: "../image/icon/html.svg",
+    },
+    {
+        id: 2,
+        title: "CSS",
+        icon: "../image/icon/css.svg",
+    },
+    {
+        id: 3,
+        title: "SASS | SCSS",
+        icon: "../image/icon/sass.svg",
+    },
+    {
+        id: 4,
+        title: "JavaScript",
+        icon: "../image/icon/javascript.svg",
+    },
+    {
+        id: 5,
+        title: "React.js",
+        icon: "../image/icon/react.svg",
+    },
+    {
+        id: 6,
+        title: "Tailwind css",
+        icon: "../image/icon/tailwind.svg",
+    },
+    {
+        id: 7,
+        title: "TypeScript",
+        icon: "../image/icon/typescript.svg",
+    },
+    {
+        id: 8,
+        title: "Redux Toolkit",
+        icon: "../image/icon/redux.svg",
+    },
+    {
+        id: 9,
+        title: "Next.js",
+        icon: "../image/icon/nextjs.svg",
+    },
+];
+
+export const projects = [
+    {
+        id: 1,
+        title: 'Meelon',
+        image: '../image/meelon.png',
+        info: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta, consequatur dolore eius sed impedit odio magni neque voluptatem quis! Culpa, laborum? Dolore rem alias porro suscipit asperiores veritatis laborum ratione, quam eveniet deserunt natus at consequatur repudiandae dolores voluptas omnis.',
+        technology: "React.js | Antd design | Redux Toolkit | Rest Api",
+        github: 'Github',
+        site: 'Website'
+    },
+    {
+        id: 2,
+        title: 'SM CARGO',
+        image: '../image/cargo.png',
+        info: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta, consequatur dolore eius sed impedit odio magni neque voluptatem quis! Culpa, laborum? Dolore rem alias porro suscipit asperiores veritatis laborum ratione, quam eveniet deserunt natus at consequatur repudiandae dolores voluptas omnis.',
+        technology: "HTML | SCSS | JavaScript | Rest Api",
+        github: 'https://github.com/MaxmudAxmedov/Cargo-lending',
+        site: 'https://smcargo.kg/'
+    },
+]
